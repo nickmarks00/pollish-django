db976af850c23e508d821d087341117ada4cf50a
nickmarks00 <nickmarks00@gmail.com>
2022-06-28 17:37:43 +1000

    Remove non-existing envvar from prod.py

--- pollish_dj/settings/prod.py
+++ pollish_dj/settings/prod.py
@@ -21,7 +21,7 @@ WSGI_APPLICATION = 'pollish_dj.wsgi.application'
 # SMTP and Emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('HOST_EMAIL')
-EMAIL_HOST_PASSWORD = config('HOST_EMAIL_PASSWORD')
+# EMAIL_HOST_USER = config('HOST_EMAIL')
+# EMAIL_HOST_PASSWORD = config('HOST_EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
