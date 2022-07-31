7b9484829a4a094bcb4c9763ad7bb9bbdf89b768
Restyled.io <commits@restyled.io>
2022-07-31 23:45:44 +0000

    Restyled by reorder-python-imports

--- pollish_dj/urls.py
+++ pollish_dj/urls.py
@@ -2,7 +2,8 @@ import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include
+from django.urls import path
 from django.views.generic import TemplateView
 
 from pollish.playground import PlaygroundView
